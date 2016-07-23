b923c47dbf853cbfb5989bd6fe4cc6bddc50d9a2
aBulgakoff <a.p.bulgakoff@gmail.com>
2016-07-24 02:40:53 +0300

    added tests for addValues()

--- test/ManagingLogic/ManagingClassTest.java
+++ test/ManagingLogic/ManagingClassTest.java
@@ -2,62 +2,115 @@ package ManagingLogic;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertTrue;
+
 public class ManagingClassTest {
+    static ManagingClass managing;
+    int vaultInitSize;
+    static String[] dataForTest = new String[]{
+            "The First String",
+            "The Second String",
+            "The Third String",
+            "The Fourth String",
+            "The Fifth String",
+            "The Sixth String"
+    };
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        managing = new ManagingClass() {
+            @Override
+            public Object getLast() {
+                return null;
+            }
+
+            @Override
+            public void removeLast() {
+            }
+        };
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        dataForTest = null;
+
+    }
+
     @Before
     public void setUp() throws Exception {
-
+        vaultInitSize = managing.vault.length;
+        managing.addValues(dataForTest);
     }
 
-    @After
-    public void tearDown() throws Exception {
 
+    @Test
+    public void vaultSizeChangedAfterAddValues() throws Exception {
+        assertTrue(managing.vault.length == dataForTest.length + vaultInitSize);
     }
 
+    @Test
+    public void compareAfterAddValues() {
+        boolean areStringsEqual = false;
+        for (int i = 0; i < managing.vault.length - vaultInitSize; i++) {
+            if (managing.vault[i].equals(dataForTest[i])) {
+                areStringsEqual = true;
+            } else {
+                areStringsEqual = false;
+                break;
+            }
+        }
+        assertTrue(areStringsEqual);
+    }
+
+    @Ignore
     @Test
     public void add() throws Exception {
 
     }
 
-    @Test
-    public void addValues() throws Exception {
-
-    }
-
+    @Ignore
     @Test
     public void get() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void remove() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void size() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void capacity() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void clear() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void getLast() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void removeLast() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void iterator() throws Exception {
 
