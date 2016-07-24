75130f8d34151e18d8a37eab06ed67e66dce1fb3
aBulgakoff <a.p.bulgakoff@gmail.com>
2016-07-25 02:26:18 +0300

    added size and capacity verification after addValues() and clear()

--- test/ManagingLogic/ManagingClassTest.java
+++ test/ManagingLogic/ManagingClassTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.assertTrue;
 
 public class ManagingClassTest {
     private int vaultInitSize;
-    private static ManagingClass manager;
+    private static ManagingClass common;
     private static String[] oneStringArray;
     private static String[] fiveStringsArray;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        manager = new ManagingClass() {
+        common = new ManagingClass() {
             @Override
             public Object getLast() {
                 return null;
@@ -37,29 +37,37 @@ public class ManagingClassTest {
 
     @Before
     public void setUp() throws Exception {
-        vaultInitSize = manager.vault.length;
-        manager.addValues(fiveStringsArray);
+        vaultInitSize = common.vault.length;
+        common.addValues(fiveStringsArray);
     }
 
     @After
     public void tearDown() throws Exception {
-        manager.clear();
+        common.clear();
     }
 
     @Test
     public void vaultSizeChangesAfterAddValues() throws Exception {
-        boolean vaultSizeNotChanged = (manager.vault.length == fiveStringsArray.length);
-        manager.addValues(oneStringArray);
-        boolean vaultSizeChanged = (manager.vault.length == oneStringArray.length + vaultInitSize);
-        assertTrue(vaultSizeChanged & vaultSizeNotChanged);
-    }
-    
-    @Test
-    public void compareAfterAddValues() {
+        boolean vaultSizeNotChanged;
+        boolean vaultSizeChanged;
+        boolean isSizeEqualInputQuantity;
 
+        vaultSizeNotChanged = (common.vault.length == fiveStringsArray.length);
+        isSizeEqualInputQuantity = (common.size() == fiveStringsArray.length);
+
+        common.addValues(oneStringArray);
+        vaultSizeChanged = (common.vault.length == oneStringArray.length + vaultInitSize);
+        isSizeEqualInputQuantity
+                = isSizeEqualInputQuantity & (common.size() == fiveStringsArray.length + oneStringArray.length);
+
+        assertTrue(vaultSizeChanged & vaultSizeNotChanged & isSizeEqualInputQuantity);
+    }
+
+    @Test
+    public void contentVerificationAfterAddValues() {
         boolean areStringsEqual = false;
         for (int i = 0; i < fiveStringsArray.length - 1; i++) {
-            if (manager.vault[i].equals(fiveStringsArray[i])) {
+            if (common.vault[i].equals(fiveStringsArray[i])) {
                 areStringsEqual = true;
             } else {
                 areStringsEqual = true;
@@ -70,10 +78,23 @@ public class ManagingClassTest {
     }
 
     @Test
-    public void clear() throws Exception {
+    public void sizeAndCapacityChangesAfterClear() throws Exception {
+        final int EMPTY = 0;
         boolean isVaultEmpty = false;
-        manager.clear();
-        for (Object obj : manager.vault) {
+        boolean isCapacityEqualSize;
+        boolean isCapacityNotChanged;
+        boolean isSizeReseted;
+        int capacityBeforeClear;
+
+        capacityBeforeClear = common.capacity();
+        isCapacityEqualSize = (common.capacity() == common.size());
+
+        common.clear();
+
+        isCapacityNotChanged = (capacityBeforeClear == common.capacity());
+        isSizeReseted = (common.size() == EMPTY);
+
+        for (Object obj : common.vault) {
             if (obj != null) {
                 isVaultEmpty = false;
                 break;
@@ -81,69 +102,31 @@ public class ManagingClassTest {
                 isVaultEmpty = true;
             }
         }
-        assertTrue(isVaultEmpty);
+        assertTrue(isVaultEmpty & isCapacityEqualSize & isCapacityNotChanged & isSizeReseted);
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
         oneStringArray = null;
         fiveStringsArray = null;
+    }
+    
+    @Ignore
+    @Test
+    public void add() throws Exception {
 
     }
 
+    @Ignore
+    @Test
+    public void get() throws Exception {
 
-//    @Ignore
-//    @Test
-//    public void add() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void get() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void remove() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void size() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void capacity() throws Exception {
-//
-//    }
-//
-//
-//    @Ignore
-//    @Test
-//    public void getLast() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void removeLast() throws Exception {
-//
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void iterator() throws Exception {
-//
-//    }
+    }
 
-//    @Test
-//    public void toString() throws Exception {
-//
-//    }
+    @Ignore
+    @Test
+    public void remove() throws Exception {
+
+    }
 
 }
