00219a6f45c37aa4043c23c9e18c476f7c6c427b
aBulgakoff <a.p.bulgakoff@gmail.com>
2016-07-24 15:58:56 +0300

    addet test for clear()

--- test/ManagingLogic/ManagingClassTest.java
+++ test/ManagingLogic/ManagingClassTest.java
@@ -5,20 +5,15 @@ import org.junit.*;
 import static org.junit.Assert.assertTrue;
 
 public class ManagingClassTest {
-    static ManagingClass managing;
-    int vaultInitSize;
-    static String[] dataForTest = new String[]{
-            "The First String",
-            "The Second String",
-            "The Third String",
-            "The Fourth String",
-            "The Fifth String",
-            "The Sixth String"
-    };
+    private int vaultInitSize;
+    private static ManagingClass manager;
+    private static String[] sixStringArray;
+    private static String[] fiveStringsArray;
+
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        managing = new ManagingClass() {
+        manager = new ManagingClass() {
             @Override
             public Object getLast() {
                 return null;
@@ -28,93 +23,124 @@ public class ManagingClassTest {
             public void removeLast() {
             }
         };
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        dataForTest = null;
-
+        sixStringArray = new String[]{
+                "The First String",
+                "The Second String",
+                "The Third String",
+                "The Fourth String",
+                "The Fifth String",
+                "The Sixth String"
+        };
+        fiveStringsArray = new String[]{
+                "The First String",
+                "The Second String",
+                "The Third String",
+                "The Fourth String",
+                "The Fifth String",
+        };
     }
 
     @Before
     public void setUp() throws Exception {
-        vaultInitSize = managing.vault.length;
-        managing.addValues(dataForTest);
+        vaultInitSize = manager.vault.length;
+        manager.addValues(sixStringArray);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        manager.clear();
     }
 
 
     @Test
     public void vaultSizeChangedAfterAddValues() throws Exception {
-        assertTrue(managing.vault.length == dataForTest.length + vaultInitSize);
+        assertTrue(manager.vault.length == sixStringArray.length + vaultInitSize);
     }
 
     @Test
     public void compareAfterAddValues() {
         boolean areStringsEqual = false;
-        for (int i = 0; i < managing.vault.length - vaultInitSize; i++) {
-            if (managing.vault[i].equals(dataForTest[i])) {
+        for (int i = 0; i < sixStringArray.length - 1; i++) {
+            if (manager.vault[i].equals(sixStringArray[i])) {
                 areStringsEqual = true;
             } else {
-                areStringsEqual = false;
+                areStringsEqual = true;
                 break;
             }
         }
         assertTrue(areStringsEqual);
     }
 
-    @Ignore
-    @Test
-    public void add() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void get() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void remove() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void size() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void capacity() throws Exception {
-
-    }
-
-    @Ignore
     @Test
     public void clear() throws Exception {
+        boolean isVaultEmpty = false;
+        manager.clear();
+        for (Object obj : manager.vault) {
+            if (obj != null) {
+                isVaultEmpty = false;
+                break;
+            } else {
+                isVaultEmpty = true;
+            }
+        }
+        assertTrue(isVaultEmpty);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        sixStringArray = null;
 
     }
 
-    @Ignore
-    @Test
-    public void getLast() throws Exception {
 
-    }
-
-    @Ignore
-    @Test
-    public void removeLast() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void iterator() throws Exception {
-
-    }
+//    @Ignore
+//    @Test
+//    public void add() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void get() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void remove() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void size() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void capacity() throws Exception {
+//
+//    }
+//
+//
+//    @Ignore
+//    @Test
+//    public void getLast() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void removeLast() throws Exception {
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void iterator() throws Exception {
+//
+//    }
 
 //    @Test
 //    public void toString() throws Exception {
